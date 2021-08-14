c5c0079f384cab777cd85e6128009dcd8c36bb67
sckimclass <71750215+sckimclass@users.noreply.github.com>
2021-08-14 10:40:59 +0900

    Create method.py

---
+++ getpost/method.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/method', methods=['GET'])
+def method():
+    if request.method == 'GET':
+        id = request.args["id"]
+        password = request.args.get("password")
+        return "data ({}, {})".format(id, password)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
